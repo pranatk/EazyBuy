9134e51ab5157a6c19f2ff7d586f71d2afa12085
Pranat Karode <pranatkarode@gmail.com>
2023-06-20 01:00:21 +0530

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,10 @@
+import {Roboto} from "next/font/google"
+const roboto = Roboto({ weight: '400',subsets: ['latin'] })
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen flex ">
+      <div className="m-auto">Hello!</div>
+    </div>
+  )
+}
